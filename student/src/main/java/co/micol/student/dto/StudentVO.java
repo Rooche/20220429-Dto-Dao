71f14df46ac467f4c5c693f83080594585e0b6a4
admin <admin@YD2-21>
2022-04-29 17:42:45 +0900

    20220429

--- student/src/main/java/co/micol/student/dto/StudentVO.java
+++ student/src/main/java/co/micol/student/dto/StudentVO.java
@@ -2,6 +2,11 @@ package co.micol.student.dto;
 
 import java.sql.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+
 public class StudentVO { //Dto
 	// 멤버변수와 getter setter로만 구성되어있는게 VO
 	private String studentId;
@@ -11,53 +16,6 @@ public class StudentVO { //Dto
 	private String address;
 	private String tel;
 
-	public String getStudentId() {
-		return studentId;
-	}
-
-	public void setStudentId(String studentId) {
-		this.studentId = studentId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Date getBirthday() {
-		return birthday;
-	}
-
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
-	}
-
-	public String getMajor() {
-		return major;
-	}
-
-	public void setMajor(String major) {
-		this.major = major;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getTel() {
-		return tel;
-	}
-
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
 	
 	@Override
 	public String toString() {
